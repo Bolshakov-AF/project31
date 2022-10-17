d6c9705baa8acdf774a724a84ba40f7e54a895ef
Bolshakov-AF <114376888+Bolshakov-AF@users.noreply.github.com>
2022-10-17 15:55:53 +0900

    commit 3
    commit 3

---
+++ IntegerArray.h
@@ -0,0 +1,52 @@
+﻿#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <assert.h> 
+
+template <class T> 
+class IntegerArray
+{
+private:
+    int m_length;
+    T* m_data;
+
+public:
+    IntegerArray()
+    {
+        m_length = 0;
+        m_data = nullptr;
+    }
+
+    IntegerArray(int length)
+    {
+        m_data = new T[length];
+        m_length = length;
+    }
+
+    ~IntegerArray()
+    {
+        delete[] m_data;
+    }
+
+    void Erase()
+    {
+        delete[] m_data;
+        m_data = nullptr;
+        m_length = 0;
+    }
+
+
+    T& operator[](int index)
+    {
+        assert(index >= 0 && index < m_length);
+        return m_data[index];
+    }
+
+    
+    int getLength(); 
+};
+
+template <typename T> 
+int IntegerArray<T>::getLength() { return m_length; } 
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+#include "IntegerArray.h"
+
+int main()
+{
+	IntegerArray<int> integerArray(8);
+	
+	for (int count = 0; count < integerArray.getLength(); ++count)
+	{
+		integerArray[count] = count;		
+	}
+
+	for (int count = integerArray.getLength() - 1; count >= 0; --count)
+		cout << integerArray[count] << endl;
+
+	return 0;
+}
